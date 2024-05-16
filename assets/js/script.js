037d5b18f57a60c7ed2fb415f2ec2399780939b6
daniel pagard <danielpagard194@gmail.com>
2024-05-16 16:31:34 +0000

    add questions and get ref inte .JS

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+const query = document.getElementById("query");
+const answer1 = document.getElementById("answer1");
+const answer2 = document.getElementById("answer2");
+const answer3 = document.getElementById("answer3");
+const answer4 = document.getElementById("answer4");
+
+let question = {};
+let chosen = true;
+let total = 0;
+let thisQuestion = 0;
+let questionsLeft = ();
+
+let questions = [
+    {
+        question: "Who is thought to be the first emperor in history?",
+        answer1: "Qin Shi Huang",
+        answer2: "Gaius Julius Caesar Augustus",
+        answer3: "Sargon of Akkad",
+        answer4: "Cyrus the Great",
+        correct: 3
+    }
+    {
+        question: "By what name have the city of Istanbul NOT been known?",
+        answer1: "Mikligarðr",
+        answer2: "Halicarnassus",
+        answer3: "Byzantium",
+        answer4: "Lygos",
+        correct: 2
+    }
+    {
+        question: "When did the great Jewish Revolt start?",
+        answer1: "66 CE",
+        answer2: "1632 CE",
+        answer3: "403 BCE",
+        answer4: "237 CE",
+        correct: 1
+    }
+    {
+        qusetion: "What year was the Battle of Thermopylae",
+        answer1: "550 BC",
+        answer2: "480 BC",
+        answer3: "310 BC",
+        answer4: "Fictonal",
+        correct: 2
+    }
+    
+]
